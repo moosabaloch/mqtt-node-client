30d4ef827ebdffc3704655da71dbf0257450888d
moosabaloch <moosa.bh@gmail.com>
2016-11-27 04:23:47 +0500

    Device state + Patient Id

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const http = require('http')  
 const port = 3000;
 
+// SUBSCRIPTION TOPICS
+const DEVICE_ACTIVITY = "onoff";
+
+
+/////// DEVICE STATE AND PATIENT ID /////
+var PATIENT_ID = "null";
+var DEVICE_CURRENT_STATE = false;
+var PATIENT = "";
+/////////////////////////////////////////
+
+
 var mqtt    = require('mqtt');
 var client  = mqtt.connect('mqtt://192.168.8.102');
+
+
 
 const requestHandler = (request, response) => {  
   console.log(request.url)
@@ -11,26 +24,67 @@ const requestHandler = (request, response) => {
 
 const server = http.createServer(requestHandler)
 
-client.publish('mqtt__topic', "some good mqtt message ",{qos: 0 , retain: false   }, ()=> console.log('GOT CB_MQTT'))
-var a = 0
-setInterval(() => client.publish('mqtt_loop', "count "+ a++ ,{qos:0, retain: false}, ()=> console.log('loop 200' + a) ), 1000);
 server.listen(port, (err) => {  
   if (err) {
     return console.log('something bad happened', err)
   }
 
   console.log(`server is listening on ${port}`)
-})
+  console.log(client.options.clientId);
+  PATIENT = client.options.clientId;
+});
+//////// SERVER CONNECTION END ////////////////
 
-client.subscribe('message');
- 
+
+
+
+/////// Publish data to server start ////////////
+client.publish('mqtt__topic', "some good mqtt message ",{qos: 0 , retain: false   }, function () {
+  console.log('GOT CB_MQTT');
+} )
+/////// Publish data to server end /////////////
+
+
+////// Start loop for data send to server /////////
+var a = 0
+setInterval(function () {
+ if (DEVICE_CURRENT_STATE){
+  client.publish('mqtt_loop', "{'temp':'23','humd':'72','pulse':'542', 'p_id':'"+PATIENT_ID+"'}" ,{qos:0, retain: false}, ()=> console.log('loop 200' + a) )
+a++; 
+}
+} , 1000);
+////// End Loop for data send to server //////////
+
+
+
+
+
+
+/////// Subscribe for msg topic START ///////////////
+client.subscribe(PATIENT); 
+client.subscribe(DEVICE_ACTIVITY); 
+
 client.on('message', function (topic, message) {
   // message is Buffer
-  console.log("Message From Server: "+message.toString());
-  client.end();
+  switch(topic.toString()){
+    case DEVICE_ACTIVITY:
+      
+      console.log("Server Message for Device activity is : "+message.toString());    
+      DEVICE_CURRENT_STATE = message.toString()=="true";
+      console.log("Device current state is : "+DEVICE_CURRENT_STATE);
+     
+    break;
+    case PATIENT:
+      console.log("Server Message for Patient ID is : "+message.toString());
+      PATIENT_ID = message.toString();
+    break;
+
+    default:
+      console.log("Topic with no action : "+topic.toString());
+    break;
+  }
+  // client.end(); //  if no more activity required
 });
+/////// Subscribe for msg topic END ///////////////
 
 
-// TODO: 
-//json string se data send karna hai publisher ko,
-//
